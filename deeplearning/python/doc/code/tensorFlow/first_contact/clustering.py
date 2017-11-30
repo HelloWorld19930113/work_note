1224a451571727298b7e0e583a664fee8b85f427
klm <guozhijie_2006@126.com>
2017-11-30 21:58:28 +0800

    cluster

--- deeplearning/python/doc/code/tensorFlow/first_contact/clustering.py
+++ deeplearning/python/doc/code/tensorFlow/first_contact/clustering.py
@@ -1,4 +1,4 @@
-#coding="utf-8"
+# coding=utf-8
 
 import numpy as np
 
@@ -17,8 +17,55 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-df = pd.DataFrame({"x": [v[0] for v in vectors_set],
-                   "y": [v[1] for v in vectors_set],
-                   })
-sns.lmplot("x","y",data=df, fit_reg=False, size=6)
+# df = pd.DataFrame({"x": [v[0] for v in vectors_set],
+#                    "y": [v[1] for v in vectors_set],
+#                    })
+# sns.lmplot("x","y",data=df, fit_reg=False, size=6)
+# plt.show()
+
+
+import tensorflow as tf
+vectors = tf.constant(vectors_set)
+k = 4
+centroides = tf.Variable(tf.slice(tf.random_shuffle(vectors),[0,0], [k,-1]))
+expanded_vectors = tf.expand_dims(vectors, 0)
+expanded_centroides = tf.expand_dims(centroides, 1)
+
+# tf.concat的两个参数位置换了
+distance = tf.reduce_sum(tf.square(tf.subtract(expanded_vectors, expanded_centroides)), 2)
+assignments = tf.argmin(distance, 0)
+
+means = tf.concat([
+    tf.reduce_mean(
+        tf.gather(vectors,
+                tf.reshape(
+                  tf.where(
+                    tf.equal(assignments, c)
+                  ),[1,-1])
+               ),reduction_indices=[1])
+    for c in xrange(k)], 0)
+
+
+update_centroides = tf.assign(centroides, means)
+
+init_op = tf.global_variables_initializer()
+
+sess = tf.Session()
+sess.run(init_op)
+
+for step in range(10):
+    _,centroid_vals,assignment_values = sess.run([update_centroides, centroides, assignments])
+
+
+data = {"x": [], "y": [], "cluster": []}
+for i in xrange(len(assignment_values)):
+  data["x"].append(vectors_set[i][0])
+  data["y"].append(vectors_set[i][1])
+  data["cluster"].append(assignment_values[i])
+
+df = pd.DataFrame(data)
+df = pd.DataFrame(data)
+sns.lmplot("x", "y", data=df,
+           fit_reg=False, size=7,
+           hue="cluster", legend=False)
 plt.show()
