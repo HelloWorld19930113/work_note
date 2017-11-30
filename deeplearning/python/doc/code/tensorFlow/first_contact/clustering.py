3f12c1e362ac5c385a311d1c8fd51520717f3314
eric <guozhijie_2006@126.com>
2017-11-30 12:59:30 +0800

    sdd first contact-clustering-1

---
+++ deeplearning/python/doc/code/tensorFlow/first_contact/clustering.py
@@ -0,0 +1,24 @@
+#coding="utf-8"
+
+import numpy as np
+
+
+num_points = 2000
+vectors_set = []
+for i in range(num_points):
+    if np.random.random() > 0.5:
+        vectors_set.append([np.random.normal(0.0, 0.9), np.random.normal(0.0, 0.9)])
+    else:
+        vectors_set.append([np.random.normal(3.0, 0.5), np.random.normal(1.0, 0.5)])
+
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+df = pd.DataFrame({"x": [v[0] for v in vectors_set],
+                   "y": [v[1] for v in vectors_set],
+                   })
+sns.lmplot("x","y",data=df, fit_reg=False, size=6)
+plt.show()
